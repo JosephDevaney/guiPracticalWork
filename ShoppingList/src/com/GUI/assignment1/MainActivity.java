13839f03f7bb3e15f819113745c23d1ab2f1a037
JosephDevaney <joe.devaney1@gmail.com>
2014-11-12 00:06:15 +0000

    added input fields to first intent; created array of jobs

--- ShoppingList/src/com/GUI/assignment1/MainActivity.java
+++ ShoppingList/src/com/GUI/assignment1/MainActivity.java
@@ -2,10 +2,20 @@ package com.GUI.assignment1;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	EditText name, age, availSpend;
+	TextView genderLabel;
+	AutoCompleteTextView jobs;
+	Spinner jobList;
+	RadioGroup gender;
+	RadioButton male, female;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
