11b4faccef8b2d3a93e244a2d7add9a6fb4b4587
JosephDevaney <joe.devaney1@gmail.com>
2014-11-16 00:47:19 +0000

    Tried to add functionality to buttons, it doesnt work. Added Row class

--- ShoppingList/src/com/GUI/assignment1/ChooseItemActivity.java
+++ ShoppingList/src/com/GUI/assignment1/ChooseItemActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,6 +18,7 @@ public class ChooseItemActivity extends ListActivity
 	{
 		Context context;
 	    String[] data;
+	    Row[] rows;
 	    private LayoutInflater inflater = null;
 
 		@SuppressWarnings("unchecked")
@@ -26,26 +28,51 @@ public class ChooseItemActivity extends ListActivity
 			// TODO Auto-generated constructor stub
 			this.context = context;
 			this.data = data;
+			
+			rows = new Row[data.length];
+			
 			inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		}
 		
 		@Override
-		public View getView(int pos, View convertView, ViewGroup parent)
+		public View getView(final int pos, View convertView, ViewGroup parent)
 		{
 			View v = convertView;
 			if (v == null)
 			{
 				v = inflater.inflate(R.layout.row, parent, false);
 			}
-
+/*
 			TextView item = (TextView) v.findViewById(R.id.item);
+			TextView price = (TextView) v.findViewById(R.id.price);
 			Button plus = (Button) v.findViewById(R.id.plus);
 			EditText amount = (EditText) v.findViewById(R.id.num);
-			Button minus = (Button) v.findViewById(R.id.minus);
+			Button minus = (Button) v.findViewById(R.id.minus);*/
+			rows[pos].item = (TextView) v.findViewById(R.id.item);
+			rows[pos].price = (TextView) v.findViewById(R.id.price);
+			rows[pos].plus = (Button) v.findViewById(R.id.plus);
+			rows[pos].amount = (EditText) v.findViewById(R.id.num);
+			rows[pos].minus = (Button) v.findViewById(R.id.minus);
 			
 
-			item.setText(data[pos]);
+			rows[pos].item.setText(data[pos]);
+			rows[pos].price.setText("4.99");	//make random
+			rows[pos].plus.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					
+					
+					String amt = rows[pos].amount.getText().toString();
+					int iAmt = Integer.parseInt(amt);
+					iAmt++;
+					rows[pos].amount.setText(Integer.toString(iAmt));
+				}
+			});
 			
+			
+			rows[pos].amount.setText("0");
 			return v;
 		}
 	}
---
+++ ShoppingList/src/com/GUI/assignment1/Row.java
@@ -0,0 +1,20 @@
+package com.GUI.assignment1;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Row 
+{
+	TextView item;
+	TextView price;
+	Button plus;
+	EditText amount;
+	Button minus;
+	
+	public Row()
+	{
+		
+	}
+
+}
